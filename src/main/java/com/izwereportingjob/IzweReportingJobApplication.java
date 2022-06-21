8733ed76f9a49a983371cd39123ef52c746008a9
MututwaM <mututwa.mututwa@yahoo.com>
2022-06-21 11:59:45 +0200

    save changes

---
+++ src/main/java/com/izwereportingjob/IzweReportingJobApplication.java
@@ -0,0 +1,13 @@
+package com.izwereportingjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzweReportingJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzweReportingJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/izwereportingjob/IzweReportingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izwereportingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzweReportingJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
